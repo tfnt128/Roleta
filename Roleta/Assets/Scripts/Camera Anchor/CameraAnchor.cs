7bb7bd10f9aab2b8fa318e93705cbc1757e664c6
Thiago Fontoura <thiagoatf@hotmail.com>
2023-11-14 20:07:45 -0300

    Anchor Script
    - Scripts de anchor para a câmera e objetos que aparecem no viewport.

---
+++ Roleta/Assets/Scripts/Camera Anchor/CameraAnchor.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraAnchor : MonoBehaviour
+{
+	public enum AnchorType
+	{
+		BottomLeft,
+		BottomCenter,
+		BottomRight,
+		MiddleLeft,
+		MiddleCenter,
+		MiddleRight,
+		TopLeft,
+		TopCenter,
+		TopRight,
+	};
+	public AnchorType anchorType;
+	public Vector3 anchorOffset;
+
+	IEnumerator updateAnchorRoutine; 
+	
+	void Start()
+	{
+		updateAnchorRoutine = UpdateAnchorAsync();
+		StartCoroutine(updateAnchorRoutine);
+	}
+
+	IEnumerator UpdateAnchorAsync()
+	{
+		uint cameraWaitCycles = 0;
+		while (ViewportHandler.Instance == null)
+		{
+			++cameraWaitCycles;
+			yield return new WaitForEndOfFrame();
+		}
+		UpdateAnchor();
+		updateAnchorRoutine = null;
+	}
+
+	void UpdateAnchor()
+	{
+		switch (anchorType)
+		{
+			case AnchorType.BottomLeft:
+				SetAnchor(ViewportHandler.Instance.BottomLeft);
+				break;
+			case AnchorType.BottomCenter:
+				SetAnchor(ViewportHandler.Instance.BottomCenter);
+				break;
+			case AnchorType.BottomRight:
+				SetAnchor(ViewportHandler.Instance.BottomRight);
+				break;
+			case AnchorType.MiddleLeft:
+				SetAnchor(ViewportHandler.Instance.MiddleLeft);
+				break;
+			case AnchorType.MiddleCenter:
+				SetAnchor(ViewportHandler.Instance.MiddleCenter);
+				break;
+			case AnchorType.MiddleRight:
+				SetAnchor(ViewportHandler.Instance.MiddleRight);
+				break;
+			case AnchorType.TopLeft:
+				SetAnchor(ViewportHandler.Instance.TopLeft);
+				break;
+			case AnchorType.TopCenter:
+				SetAnchor(ViewportHandler.Instance.TopCenter);
+				break;
+			case AnchorType.TopRight:
+				SetAnchor(ViewportHandler.Instance.TopRight);
+				break;
+		}
+	}
+
+	void SetAnchor(Vector3 anchor)
+	{
+		Vector3 newPos = anchor + anchorOffset;
+		if (!transform.position.Equals(newPos))
+		{
+			transform.position = newPos;
+		}
+	}
+
+#if UNITY_EDITOR
+	void Update()
+	{
+		if (updateAnchorRoutine == null)
+		{
+			updateAnchorRoutine = UpdateAnchorAsync();
+			StartCoroutine(updateAnchorRoutine);
+		}
+	}
+#endif
+}
